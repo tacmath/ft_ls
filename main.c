1b1287020e2ce3a530a8a431572c1ed5e34005ed
tac <tacmathys@gmail.com>
2019-05-03 23:55:23 +0200

    sort

--- main.c
+++ main.c
@@ -117,14 +117,20 @@ int fill_files(char *path, t_dir **files, char *flag)
 	return (1);
 }
 
-int	ft_strcmp(char *s1, char *s2)
+int	name_cmp(char *s1, char *s2)
 {
-	int n;	
+	int n;
+	int m;
 	
 	n = -1;
-	while (s1[++n] == s2[n] && s1[n] && s2[n])
+	m = -1;
+	if (s1[0] == '.')
+		n++;
+	if (s2[0] == '.')
+		m++;
+	while (s1[++n] == s2[++m] && s1[n] && s2[m])
 		;
-	return (s1[n] - s2[n]);
+	return (s1[n] - s2[m]);
 }
 
 void	sort_by_name(t_dir *files)
@@ -132,68 +138,143 @@ void	sort_by_name(t_dir *files)
 	t_dir	tmp;
 	int		n;
 	int		m;
-	int		ret1;
-	int		ret2;
 
 	m = -1;
 	while (files[++m].name)
 	{
 		n = 0;
 		while (files[++n].name)
-		{
-			if (files[n - 1].name[0] == '.')
-				ret1 = 1;
-			else
-				ret1 = 0;
-			if (files[n].name[0] == '.')
-				ret2 = 1;
-			else
-				ret2 = 0;
-			if (ft_strcmp(&(files[n - 1].name[ret1]), &(files[n].name[ret2])) > 0)
+			if (name_cmp(files[n - 1].name, files[n].name) > 0)
 			{
 				tmp = files[n - 1];
 				files[n - 1] = files[n];
 				files[n] = tmp;
 			}
-		}
 	}
 }
-/*
+
 void	sort_by_time(t_dir *files, struct stat *stats)
 {
-	char	*tmp_name;
-	int	tmp_type;
+	t_dir		tmp;
+	struct stat	tmp_stat;
 	int		n;
 	int		m;
 
 	m = -1;
-	while (files[++m])
+	while (files[++m].name)
 	{
 		n = 0;
-		while (files[++n])
-			if (stats)
+		while (files[++n].name)
+			if (stats[n - 1].st_mtime < stats[n].st_mtime ||
+			(stats[n - 1].st_mtime == stats[n].st_mtime &&
+			files[n - 1].name > files[n].name))
 			{
-				tmp_name = files[n - 1].name;
-				tmp_type = files[n - 1].type;
-				files[n - 1].name = files[n].name;
-				files[n - 1].type = files[n].type;
-				files[n].name = tmp_name;
-				files[n].type = tmp_type;
+				tmp = files[n - 1];
+				tmp_stat = stats[n - 1];
+				files[n - 1] = files[n];
+				stats[n - 1] = stats[n];
+				files[n] = tmp;
+				stats[n] = tmp_stat;
 			}
 	}
-}*/
+}
+
+int ft_strlen(char *str)
+{
+	int n;
+
+	n = -1;
+	while (str[++n])
+		;
+	return (n);
+}
+
+char *add_to_path(char *path, char *add)
+{
+	char *new;	
+	int n;
+	int m;
+	
+	if (!(new = malloc(sizeof(char) * (ft_strlen(path) + ft_strlen(add) + 2))))
+		return (0);
+	n = -1;
+	while (path[++n])
+		new[n] = path[n];
+	if (path[n - 1] != '/')
+		new[n++] = '/';
+	m = -1;
+	while (add[++m])
+		new[n++] = add[m];
+	new[n] = 0;
+	return (new);
+}
+
+int get_stats(char *path, t_dir *files, struct stat **stats)
+{
+	int n;
+	char *tmp;
+
+	n = -1;
+	while (files[++n].name)
+		;
+	if (!(*stats = malloc(sizeof(struct stat) * n)))
+		return (0);
+	n = -1;
+	while (files[++n].name)
+	{
+		if (!(tmp = add_to_path(path, files[n].name)))
+			return (0);
+		stat(tmp, &(*stats)[n]);
+		free(tmp);
+	}
+	return (1);
+}
+
+void rev_all(t_dir *files, struct stat *stats)
+{
+	t_dir tmp;
+	struct stat tmp_stat;
+	int len;
+	int n;
+
+	len = -1;
+	while (files[++len].name)
+		;
+	len--;
+	n = -1;
+	while (++n < (len + 1) / 2)
+	{
+		tmp = files[n];
+		tmp_stat = stats[n];
+		files[n] = files[len - n];
+		stats[n] = stats[len - n];
+		files[len - n] = tmp;
+		stats[len - n] = tmp_stat;
+	}
+}
 
 int list_files(char *flag, char *path)
 {
 	t_dir	*files;
+	struct stat *stats;
 	int n;
 
 	if (!fill_files(path, &files, flag))
 		return (0);
-	sort_by_name(files);
+	if (!(get_stats(path, files, &stats)))
+		return (0);
+	if (!flag['t'])
+		sort_by_name(files);
+	else
+		sort_by_time(files, stats);
+	if (flag['r'])
+		rev_all(files, stats);
 	n = -1;
 	while (files[++n].name)
 		printf("%s\n", files[n].name);
+	t_dir_free(&files);
+	free(stats);
+	free(path);
 	return (1);
 }
 
